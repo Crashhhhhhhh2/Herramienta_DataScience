389f250d46d71cafb6dce3b580123c5157b511ae
Crashhhhhhhh2 <david.bermudez@credibanco.com>
2026-03-02 10:39:23 -0500

    Add files via upload

---
+++ _next/static/dKcLPWtB0BdL3K6jmLaPO/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Herramienta-de-planeacion//_next/:path+",
+        "destination": "/Herramienta-de-planeacion/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
